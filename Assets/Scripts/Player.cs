e8745d8a1012a3594aa10e73e5a9227a3a434a0f
Michael Goff <mhgoff01@louisville.edu>
2026-06-11 01:26:27 -0400

    Revert "h"
    This reverts commit 6a1c6ebf5521f801f45c4b4f37e5ad2cc9eb3d18, reversing
    changes made to c6132b568e11cf146fac00662802863dc2771262.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Collections;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Tilemaps;
 
-public class Player : MonoBehaviour, IAttacker
+public class Player : MonoBehaviour
 {
-    [Header("Input"), Space]
     [SerializeField] InputActionReference moveAction;
     [SerializeField] InputActionReference dashAction;
     [SerializeField] InputActionReference attackAction;
-    [SerializeField] InputActionReference shootAction;
     [SerializeField] InputActionReference jumpAction;
 
-    [Header("Player Properties"), Space]
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float dashSpeed = 10f;
@@ -22,15 +16,12 @@ public class Player : MonoBehaviour, IAttacker
     [SerializeField] float dashDistance = 1f;
     [SerializeField] float fallMultiplier = 2f;
 
-    [Header("Ground Check"), Space]
-    [SerializeField] Transform groundDetector;
-    [SerializeField] float groundCheckDistance = 0.2f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] LayerMask groundLayer;
 
     Rigidbody2D rb;
     CapsuleCollider2D capsuleCollider;
-    Fighter fighter;
-    IAttacker boss;
     Vector2 moveDirection;
     Vector2 jumpDirection;
     bool isGrounded;
@@ -39,22 +30,16 @@ public class Player : MonoBehaviour, IAttacker
     {
         dashAction.action.performed += OnDash;
         attackAction.action.started += OnAttack;
-        shootAction.action.started += OnShoot;
         jumpAction.action.started += OnJump;
     }
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         capsuleCollider = GetComponent<CapsuleCollider2D>();
-        fighter = GetComponent<Fighter>();
     }
-    void Start()
-    {
-        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<IAttacker>();
-    }
+
     private void Update()
     {
         moveDirection.x = moveAction.action.ReadValue<Vector2>().x;
@@ -70,16 +55,7 @@ public class Player : MonoBehaviour, IAttacker
 
     private void CheckIsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.Raycast(groundDetector.position, Vector2.down, groundCheckDistance, groundLayer);
-        if (raycastHit2D.collider is TilemapCollider2D)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
     private void OnJump(InputAction.CallbackContext context)
     {
@@ -98,35 +74,11 @@ public class Player : MonoBehaviour, IAttacker
         else
         {
             Debug.Log("Attacking normally.");
-            fighter.MeleeAttack();
         }
-    }
-
-    private void OnShoot(InputAction.CallbackContext context)
-    {
-        Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 direction = (mouseWorldPosition - (Vector2)transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        fighter.RangedAttackManual(direction, angle);
     }
 
     private void OnDash(InputAction.CallbackContext context)
     {
-        Vector2 dashPosition = rb.position + moveDirection * dashSpeed;
-        rb.MovePosition(dashPosition);
-        //StartCoroutine(Dash());
-    }
-
-    private IEnumerator Dash()
-    {
-        float originalGravity = rb.gravityScale;
-        rb.gravityScale = 0;
-        rb.linearVelocity = Vector2.zero;
-        rb.linearVelocityX = dashSpeed * moveDirection.x;
-        yield return new WaitForSeconds(dashDuration);
-
-        rb.gravityScale = originalGravity;
 
     }
 
@@ -146,15 +98,5 @@ public class Player : MonoBehaviour, IAttacker
         dashAction.action.performed -= OnDash;
         attackAction.action.started -= OnAttack;
         jumpAction.action.started -= OnJump;
-        shootAction.action.started -= OnShoot;
-    }
-
-    public Health GetHealth()
-    {
-        if (TryGetComponent(out Health health))
-        {
-            return health;
-        }
-        return null;
     }
 }
